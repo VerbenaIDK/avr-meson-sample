4b35fea1881cb4e7f449bbd67ecf26112c2ecf6b
verbenaidk <verbenacomms@gmail.com>
2026-04-22 22:13:01 -0300

    start woooo

---
+++ README.md
@@ -0,0 +1,7 @@
+# sample project for AVR on Meson
+
+that simple
+tested on and for a knock-off Arduino UNO R3 with some weird possibly ATMega clone???
+idk
+
+im tired
---
+++ src/main.c
@@ -0,0 +1,16 @@
+#define __AVR_ATmega328PB__
+#define F_CPU 16000000UL
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main() {
+    DDRB |= (1 << PB5);
+    
+    while (1) {
+        PORTB |= (1 << PB5);
+        _delay_ms(500);
+        PORTB &= ~(1 << PB5);
+        _delay_ms(500);
+    }
+}
